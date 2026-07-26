f97c6a58e053e17ebb3ae6666f43a38ad606ad84
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-07-26 13:25:27 +0530

    Create Fibonacci.java

---
+++ Number_series/Fibonacci.java
@@ -0,0 +1,19 @@
+package java_practice;
+import java.util.Scanner;
+public class Fibonacci_series {
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		fibonacci(n);
+	}
+	public static void fibonacci(int n) {
+		int a=0;
+		int b=1;
+		for(int i=1;i<=n;i++) {
+			System.out.print(a+" ");
+			int c=a+b;
+			a=b;
+			b=c;
+		}
+	}
+}
