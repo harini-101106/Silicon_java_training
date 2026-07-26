1de06a97eb93924a93e489e5ca0948181591d24b
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-07-26 13:28:18 +0530

    Create Star_pattern.java

---
+++ Number_series/Star_pattern.java
@@ -0,0 +1,14 @@
+package java_practice;
+import java.util.Scanner;
+public class Star_pattern {
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
