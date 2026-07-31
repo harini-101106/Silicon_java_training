e110f5dd50d10f26026e32e7fcf7662b8fd43f29
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-07-31 21:10:50 +0530

    Create CheckSorted.java

---
+++ Arrays/CheckSorted.java
@@ -0,0 +1,28 @@
+package Arrays;
+import java.util.Scanner;
+public class CheckSorted {
+	public static boolean sort(int []arr,int n) {
+		for(int i=0;i<n;i++) {
+			for(int j=i+1;j<n;j++) {
+				if(arr[i]>arr[j]) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int []arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		if(sort(arr,n)) {
+			System.out.println("Sorted");
+		}
+		else {
+			System.out.println("Not sorted");
+		}
+	}
+}
