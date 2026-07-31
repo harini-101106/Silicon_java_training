1c7a036ddb93e0245b915420c660b12b8357f869
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-07-31 20:12:13 +0530

    Create Occurence.java

---
+++ Arrays/Occurence.java
@@ -0,0 +1,29 @@
+package Arrays;
+import java.util.Scanner;
+public class Occurence {
+	public static void occur(int []arr,int n) {
+		boolean []visited=new boolean[n];
+		for(int i=0;i<n;i++) {
+			if(visited[i]) {
+				continue;
+			}
+			int count=1;
+			for(int j=i+1;j<n;j++) {
+				if(arr[i]==arr[j]) {
+					count++;
+					visited[j]=true;
+				}
+			}
+			System.out.println(arr[i]+" -"+count);
+		}
+	}
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int []arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		occur(arr,n);
+	}
+}
