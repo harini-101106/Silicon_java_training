7a572e141dfcb572b20426d3d1fbdb1cc07504a1
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-07-31 20:06:47 +0530

    Create FirstNonrepeating.java

---
+++ Arrays/FirstNonrepeating.java
@@ -0,0 +1,32 @@
+package Arrays;
+import java.util.Scanner;
+public class FirstNonrepeating {
+	public static void firstNonrep(int []arr,int n) {
+		boolean []visited=new boolean[n];
+		for(int i=0;i<n;i++) {
+			if(visited[i]) {
+				continue;
+			}
+			int count=1;
+			for(int j=i+1;j<n;j++) {
+				if(arr[i]==arr[j]) {
+					count++;
+					visited[j]=true;
+				}
+			}
+			if(count==1) {
+				System.out.print(arr[i]+" ");
+			return;
+			}
+		}
+	}
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int []arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		firstNonrep(arr,n);
+	}
+}
