f3347423144de6fda030230ee344b13ed31e09cc
Harini Thirumurugan <harinithirumurugan2006@gmail.com>
2026-07-31 20:17:11 +0530

    Create Swapping.java

---
+++ Arrays/Swapping.java
@@ -0,0 +1,23 @@
+package Arrays;
+import java.util.Scanner;
+public class Swapping {
+	public static void swap(int []arr,int n) {
+		for(int i=0,j=n-1;i<j;i++,j--) {
+			int temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+		}
+	}
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int []arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		swap(arr,n);
+	for(int i=0;i<n;i++) {
+		System.out.print(arr[i]+" ");
+	}
+	}
+}
